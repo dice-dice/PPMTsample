befe1a33296c3086cedff385897a76ec872993bb
hatakeyama <z7daisuke.debzzha@gmail.com>
2024-02-08 20:15:38 +0900

    packagejson change old version

--- ppmtool-react-client/src/store.js
+++ ppmtool-react-client/src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initalState = {};
@@ -13,8 +13,8 @@ if (window.navigator.userAgent.includes("Chrome")) {
     initalState,
     compose(
       applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSIONS__ &&
-        window.__EDUX_DEVTOOLS_EXTENSIONS__()
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
 } else {
